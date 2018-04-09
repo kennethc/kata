77f291c194895fad9e61d9050dbb8291c4c91b08
Kenneth Chai <chai.yau.tack@gmail.com>
2018-04-09 20:40:38 +1000

    Abstracted duplicate code

--- fizzbuzztdd/main.go
+++ fizzbuzztdd/main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-    "strconv"
-    "fmt"
+	"fmt"
+	"strconv"
 )
 
 func main() {
-    for i := 1; i <= 100; i++ {
-        fmt.Println(check(i))
-    }
+	for i := 1; i <= 100; i++ {
+		fmt.Println(check(i))
+	}
 }
 
 func check(x int) string {
-    var r string
-    if x%3 == 0 {
-        r += "Fizz"
-    }
-    if x%5 == 0 {
-        r += "Buzz"
-    }
-    if r == "" {
-        r = strconv.Itoa(x)
-    }
-    return r
+	var r string
+	if multiple(x, 3) {
+		r += "Fizz"
+	}
+	if multiple(x, 5) {
+		r += "Buzz"
+	}
+	if r == "" {
+		r = strconv.Itoa(x)
+	}
+	return r
+}
+
+func multiple(x int, m int) bool {
+	return x%m == 0
 }
