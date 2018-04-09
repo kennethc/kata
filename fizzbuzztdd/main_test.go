1d89007a2148bb814d7a25c03301a8aa43f1601a
Kenneth Chai <chai.yau.tack@gmail.com>
2018-04-09 20:33:39 +1000

    Move test cases outside of functions. Add benchmark test.

--- fizzbuzztdd/main_test.go
+++ fizzbuzztdd/main_test.go
@@ -2,25 +2,35 @@ package main
 
 import "testing"
 
-func TestCheck(t *testing.T) {
-    cases := []struct {
-        in int
-        want string
-    }{
-        {1, "1"},
-        {2, "2"},
-        {3, "Fizz"},
-        {5, "Buzz"},
-        {6, "Fizz"},
-        {10, "Buzz"},
-        {15, "FizzBuzz"},
-        {30, "FizzBuzz"},
-    }
+type testmap []struct {
+	in   int
+	want string
+}
 
-    for _, c := range cases {
-        got := check(c.in)
-        if got != c.want {
-            t.Errorf("check(%v) == %v, want %v", c.in, got, c.want)
-        }
-    }
+var (
+	cases = testmap{
+		{1, "1"},
+		{2, "2"},
+		{3, "Fizz"},
+		{5, "Buzz"},
+		{6, "Fizz"},
+		{10, "Buzz"},
+		{15, "FizzBuzz"},
+		{30, "FizzBuzz"},
+	}
+)
+
+func TestCheck(t *testing.T) {
+	for _, c := range cases {
+		got := check(c.in)
+		if got != c.want {
+			t.Errorf("check(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
+
+func BenchmarkCheck(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		check(cases[0].in)
+	}
 }
